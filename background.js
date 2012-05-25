0b4460684688d894dc32cb63d92cc7109d9c3414
Bo Zhu <zhubo03@gmail.com>
2012-05-25 01:23:39 -0400

    minor changes

--- background.js
+++ background.js
@@ -1,12 +1,13 @@
 var unblock_youku = {};  // namespace
 unblock_youku.ip_addr  = "220.181.111.";
 unblock_youku.ip_addr += Math.floor(Math.random() * 254 + 1); // 1 ~ 254
+console.log('faked ip addr: ' + unblock_youku.ip_addr);
 
 chrome.webRequest.onBeforeSendHeaders.addListener(
     // callback function
     function(details) {
         details.requestHeaders.push({
-            name: "X-Forwarded-For", 
+            name: "X-Forwarded-For",
             value: unblock_youku.ip_addr
         });
 
