2feeb7b6a87b142e1e711128e1efb5bc8a4ffceb
Bo Zhu <zhubo03@gmail.com>
2012-11-01 20:58:39 -0400

    a prototype viglink feature

--- chrome/pages/js/popup.js
+++ chrome/pages/js/popup.js
@@ -55,6 +55,28 @@ $(document).ready(function() {
     });
 
     $('div#version').html('<i class="icon-heart"></i> ' + background.unblock_youku.version);
+
+    background.get_storage('support_us', function(option) {
+        if (option === 'yes') {
+            $('#checkbox_support_us').prop('checked', true);
+        } else {
+            $('#checkbox_support_us').prop('checked', false);
+        }
+    });
+
+
+    $('#checkbox_support_us').click(function() {
+        if ($('#checkbox_support_us').prop('checked')) {
+            background.set_storage('support_us', 'yes', function() {
+                console.log('change to support us');
+            });
+        } else {
+            background.set_storage('support_us', 'no', function() {
+                console.log('change to not support us');
+            });
+        }
+    });
+
     
     // button actions
     $('button#lite').click(function() {
