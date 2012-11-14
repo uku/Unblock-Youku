7d2fa153eb7068dea991e3c06bcb3825868a77d3
Bo Zhu <github@bozhu.me>
2012-11-14 01:55:48 -0500

    also can add the info about current mode; much better for problem solving

--- chrome/pages/js/popup.js
+++ chrome/pages/js/popup.js
@@ -52,20 +52,22 @@ $(document).ready(function() {
                 $('button#normal').addClass('active');
                 break;
         }
+
+        // append system info to the wufoo feedback link
+        // such as extension version, chrome version, and os type
+        var locale = navigator.language.substr(0, 2);
+        if (locale === 'en' || locale === 'zh') {
+            // jQuery.browser is not always accurate
+            var system_info = 'Unblock Youku ' + background.unblock_youku.version;
+            system_info += ' (' + current_mode_name + ', ' + locale + '); ';
+            system_info += navigator.userAgent;
+            console.log(system_info);
+
+            var feedback_url = $('#feedback a');
+            feedback_url.prop('href', feedback_url.prop('href') + '/def/field13=' + encodeURIComponent(system_info));
+        }
     });
 
-    // append system info to the wufoo feedback link
-    // such as extension version, chrome version, and os type
-    var locale = navigator.language.substr(0, 2);
-    if (locale === 'en' || locale === 'zh') {
-        // jQuery.browser is not always accurate
-        var system_info = 'Unblock Youku ' + background.unblock_youku.version + ' (' + locale.toUpperCase() + '); ' + navigator.userAgent;
-        console.log(system_info);
-
-        var feedback_url = $('#feedback a');
-        feedback_url.prop('href', feedback_url.prop('href') + '/def/field13=' + encodeURIComponent(system_info));
-    }
-    
     // button actions
     $('button#lite').click(function() {
         console.log('to change mode to lite');
