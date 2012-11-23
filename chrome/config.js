c1aaa73f402049028c1268a246f1c8e702aef5c4
Bo Zhu <github@bozhu.me>
2012-11-23 09:59:27 -0500

    small bug fix for imanhua

--- chrome/config.js
+++ chrome/config.js
@@ -37,8 +37,8 @@ unblock_youku.normal_url_list = unblock_youku.url_list.concat([
     'http://v.pptv.com/show/*.html',
     'http://music.baidu.com/box*',
     'http://fm.baidu.com/*',
-    'http://www.imanhua.com/comic/*',
-    'http//imanhua.com/comic/*'
+    'http://imanhua.com/comic/*',
+    'http://www.imanhua.com/comic/*'
 ]);
 unblock_youku.redirect_url_list = unblock_youku.url_list;
 unblock_youku.header_extra_url_list = [
