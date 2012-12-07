4a39691b27a7fb0483e2a66a4109211926439dcd
Bo Zhu <github@bozhu.me>
2012-12-06 23:22:00 -0500

    removed useless try-catch in storage.js

--- chrome/storage.js
+++ chrome/storage.js
@@ -19,66 +19,21 @@
 
 // have to use a callback function
 function get_storage(key, callback) {
-    // there is some bug in the chrome.storage api
-    // see https://github.com/zhuzhuor/Unblock-Youku/issues/45
-    // and http://code.google.com/p/chromium/issues/detail?id=158971
-    // but we can't reproduce the similar erros
-    // so here we just try to be more carefull
-    // although we can't test if the try...catch thing works
-
-    try {
-        chrome.storage.sync.get(key, function(items) {
-            callback(items[key]);
-        });
-    } catch (err) {
-        console.error(err);
-        _gaq.push(['_trackEvent', 'Storage API Error', key]);
-        callback(localStorage[key]);
-    }
-}
-
-
-function set_storage(key, value, callback) {
-    try {
-        var obj = {};
-        obj[key] = value;  // can't just use {key: value}
-        chrome.storage.sync.set(obj, callback);
-    } catch (err) {
-        console.error(err);
-        if (key === 'unblock_youku_mode') {
-            fallback_change_mode(value, callback);
-        } else {
-            localStorage[key] = value;
-            callback();
-        }
-    }
-}
-
-
-// if the storage api fails, we couldn't count on onChange listener
-// we have to reset the mode settings by ourselves
-function fallback_change_mode(new_mode_name, callback) {
-    get_storage('unblock_youku_mode', function(old_mode_name) {
-        // only alter the settings if there is an old one
-        // in case that it's the first time install the extension
-        console.warn('fallback_change_mode changes mode ' + old_mode_name + ' -> ' + new_mode_name);
-        if (typeof old_mode_name !== 'undefined') {
-            clear_mode_settings(old_mode_name);
-            setup_mode_settings(new_mode_name);
-        }
-        localStorage.unblock_youku_mode = new_mode_name;
-        callback();
+    chrome.storage.sync.get(key, function(items) {
+        callback(items[key]);
     });
 }
 
 
+function set_storage(key, value, callback) {
+    var obj = {};
+    obj[key] = value;  // can't just use {key: value}
+    chrome.storage.sync.set(obj, callback);
+}
+
+
 function remove_storage(key, callback) {
-    try {
-        chrome.storage.sync.remove(key, callback);
-    } catch (err) {
-        localStorage.removeItem(key);
-        callback();
-    }
+    chrome.storage.sync.remove(key, callback);
 }
 
 
