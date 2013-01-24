794050504421ecc94037a7cc6430caf8f19090cc
Bo Zhu <github@bozhu.me>
2013-01-24 02:55:09 -0500

    added kugou to normal mode; seems their website is experiencing 500 error?

--- chrome/config.js
+++ chrome/config.js
@@ -41,6 +41,7 @@ unblock_youku.normal_url_list = unblock_youku.url_list.concat([
     'http://music.baidu.com/data/music/fmlink*',
     'http://music.baidu.com/song/*/download*',
     'http://fm.baidu.com/*',
+    'http://www.kugou.com/*',
 
     'http://v.pptv.com/show/*.html',
     'http://www.songtaste.com/*',
