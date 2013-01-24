2ce43f3ac81f131a92aa11233279a0ee737a6b5f
Bo Zhu <github@bozhu.me>
2013-01-24 03:15:32 -0500

    http://www.tudou.com/listplay/

--- chrome/config.js
+++ chrome/config.js
@@ -27,6 +27,7 @@ unblock_youku.backup_server = 'yo.uku.im/proxy.php';
 unblock_youku.normal_url_list = unblock_youku.url_list.concat([
     'http://www.tudou.com/programs/view/*',
     'http://www.tudou.com/albumplay/*',
+    'http://www.tudou.com/listplay/*',
 
     'http://live.video.sina.com.cn/room/*',
 
