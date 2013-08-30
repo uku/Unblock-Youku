414379defde2f2e0c4acc8995948f5fb86364e1a
Bo Zhu <github@bozhu.me>
2013-08-30 09:15:18 -0400

    added commas

--- shared/urls.js
+++ shared/urls.js
@@ -141,8 +141,8 @@ unblock_youku.server_extra_urls = [
     'http://m.letv.com/api/geturl?*',
     'http://serviceinfo.sdk.duomi.com/api/serviceinfo/getserverlist*',
     // for PC Clients only
-    'http://iplocation.geo.qiyi.com/cityjson'
-    'http://sns.video.qq.com/tunnel/fcgi-bin/tunnel*'
+    'http://iplocation.geo.qiyi.com/cityjson',
+    'http://sns.video.qq.com/tunnel/fcgi-bin/tunnel*',
     // for 3rd party's DNS for Apple TV (see pull request #78)
     'http://180.153.225.136/*',
     'http://118.244.244.124/*',
