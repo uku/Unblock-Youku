4f6090bb3ee087fe5913af5b228b62d24f94e874
Bo Zhu <github@bozhu.me>
2013-09-16 21:08:23 -0400

    added flush() to the test script for real-time logs

--- test/run-all-tests.py
+++ test/run-all-tests.py
@@ -32,9 +32,11 @@ print 'PhantomJS',
 try:
     version = subprocess.check_output(['phantomjs', '--version'])
     print version
+    sys.stdout.flush()
 except Exception as exp:
     print 'is not installed.'
     print 'Please install it and try again.'
+    sys.stdout.flush()
     sys.exit(-1)
 
 
@@ -44,27 +46,32 @@ server_process = None
 def start_server():
     global server_process
     print 'To start the server, and wait for 21 seconds to set up...'
+    sys.stdout.flush()
     server_process = subprocess.Popen(
-            ['node', '../server/server.js', '--production', '--port=8888'])
+        ['node', '../server/server.js', '--production', '--port=8888'])
     time.sleep(21)
 
 
 def stop_server():
     time.sleep(1)
     print 'To stop the server...',
+    sys.stdout.flush()
     server_process.terminate()
     server_process.wait()
     print 'done.'
+    sys.stdout.flush()
 
 
 # http://goo.gl/xaBer
 def red_alert(text):
     print "\033[7;31m" + text + "\033[0m"
+    sys.stdout.flush()
 
 
 def run_all_tests():
     print
     print 'To run all test-*.js files...'
+    sys.stdout.flush()
     num_failed = 0
     num_passed = 0
     for file_name in os.listdir('.'):
@@ -75,6 +82,7 @@ def run_all_tests():
                 command = ['phantomjs', file_name]
             print
             print ' '.join(command)
+            sys.stdout.flush()
             return_value = subprocess.call(command)
             time.sleep(2)  # sleep 2 seconds between tests
             if return_value != 0:
@@ -83,13 +91,16 @@ def run_all_tests():
             else:
                 num_passed += 1
                 print file_name + ' passed.'
+                sys.stdout.flush()
     print
+    sys.stdout.flush()
     if num_failed > 0:
         red_alert('Final results: ' + str(num_failed) + ' TESTS FAILED'
-                + ' (out of ' + str(num_failed + num_passed) + ')')
+                  + ' (out of ' + str(num_failed + num_passed) + ')')
     else:
         print 'All %d tests passed.' % (num_passed + num_failed)
     print
+    sys.stdout.flush()
     return num_failed
 
 
