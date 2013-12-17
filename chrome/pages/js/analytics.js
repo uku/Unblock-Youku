651b6ebf7d57546fe60591eaff28c7ccb0d2ed41
Bo Zhu <github@bozhu.me>
2013-12-16 21:32:18 -0500

    the new analytics code didn't work

--- chrome/pages/js/analytics.js
+++ chrome/pages/js/analytics.js
@@ -23,7 +23,7 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-30726750-7');
+ga('create', 'UA-30726750-8', 'auto');
 ga('set', 'anonymizeIp', true);
 // ga('send', 'pageview');
 
