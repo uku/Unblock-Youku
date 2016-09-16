86f9b82a3cf36b23200d9ad6ac5e3dccf7def6f0
zhuzhuor <zhuzhuor@users.noreply.github.com>
2016-09-16 09:26:23 -0700

    add https://interface.bilibili.com

--- shared/urls.js
+++ shared/urls.js
@@ -208,6 +208,7 @@ unblock_youku.chrome_proxy_urls = unblock_youku.redirect_urls.concat([
     'http://www.qie.tv/*',
     'http://www.bilibili.com/video/*',
     'http://interface.bilibili.com/*',
+    'https://interface.bilibili.com/*',
     // 'http://live-play.acgvideo.com/live/*',
     'http://m10.music.126.net/*', //for the testing of netease music
 
